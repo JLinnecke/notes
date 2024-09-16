08c40b0e601b455666682ccd5a3816ac92ad06b6
JLinnecke <jlinnecke@gmail.com>
2024-09-16 10:31:31 +0200

    install firebase-tools and angular fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"danotes-68a3c","appId":"1:248709517966:web:dea16e921e929e9851efdf","storageBucket":"danotes-68a3c.appspot.com","apiKey":"AIzaSyAVPo8vEyYkGA_dkJjqdJCpDWlkwI-9Xls","authDomain":"danotes-68a3c.firebaseapp.com","messagingSenderId":"248709517966"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
