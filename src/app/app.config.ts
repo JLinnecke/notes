8182bb69aa03f6ddc2fb4ba06735922b6d5def75
JLinnecke <jlinnecke@gmail.com>
2024-09-23 10:00:19 +0200

    reference for normal and trash notes/add access for single doc

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,5 +6,7 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"danotes-68a3c","appId":"1:248709517966:web:dea16e921e929e9851efdf","storageBucket":"danotes-68a3c.appspot.com","apiKey":"AIzaSyAVPo8vEyYkGA_dkJjqdJCpDWlkwI-9Xls","authDomain":"danotes-68a3c.firebaseapp.com","messagingSenderId":"248709517966"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers:
+  [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"danotes-68a3c","appId":"1:248709517966:web:dea16e921e929e9851efdf","storageBucket":"danotes-68a3c.appspot.com","apiKey":"AIzaSyAVPo8vEyYkGA_dkJjqdJCpDWlkwI-9Xls","authDomain":"danotes-68a3c.firebaseapp.com","messagingSenderId":"248709517966"}))),
+  importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"danotes-68a3c","appId":"1:248709517966:web:dea16e921e929e9851efdf","storageBucket":"danotes-68a3c.appspot.com","apiKey":"AIzaSyAVPo8vEyYkGA_dkJjqdJCpDWlkwI-9Xls","authDomain":"danotes-68a3c.firebaseapp.com","messagingSenderId":"248709517966"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { collectionData, Firestore, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Note } from '../interfaces/note.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+  trashNotes: Note[] = [];
+  nornmalNotes: Note[] = [];  
+
+  items$;
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    this.items$ = collectionData(this.getNormalRef());
+  }
+
+  //const itemCollection = collection(this.firestore, 'items');
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getNormalRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getSingleDocRef(colId:string, docId:string) {
+    return doc(collection(this.firestore, colId), docId);
+
+  }
 }
